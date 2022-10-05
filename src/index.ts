0b621e296881c1cd26d3027237ffa4dc419b2911
David <david.alfons@kotrada.com>
2022-10-05 10:09:38 +0200

    error handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from 'express';
+import errorMiddleware from './middleware/error.middleware';
 
 const PORT = 3000;
 // create instance from server
@@ -6,10 +7,19 @@ const app: Application = express();
 
 //add route for / path
 app.get('/', (req: Request, res: Response) => {
+  throw new Error ();
   res.json({
     message: 'Hello World',
   });
+  
 });
+
+app.use((_req: Request, res: Response) => {
+  res.status(404).json({
+    message: 'Page not found!',
+  });
+});
+app.use(errorMiddleware);
 // start express server
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
