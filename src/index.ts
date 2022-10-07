8aa5ad322b9bca91579a54c5cc1e4320415ee754
David <david.alfons@kotrada.com>
2022-10-07 14:09:14 +0200

    connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 import errorMiddleware from './middleware/error.middleware';
+import config from './config';
+import db from './database';
 
-const PORT = 3000;
+const PORT = config.port || 3000;
 // create instance from server
 const app: Application = express();
 
 //add route for / path
 app.get('/', (req: Request, res: Response) => {
-  throw new Error ();
+  throw new Error();
   res.json({
     message: 'Hello World',
   });
-  
 });
 
 app.use((_req: Request, res: Response) => {
@@ -19,6 +20,20 @@ app.use((_req: Request, res: Response) => {
     message: 'Page not found!',
   });
 });
+
+db.connect().then((client) => {
+  return client
+    .query('SELECT NOW()')
+    .then((res) => {
+      client.release();
+      console.log(res.rows);
+    })
+    .catch((err) => {
+      client.release();
+      console.log(err.stack);
+    });
+});
+
 app.use(errorMiddleware);
 // start express server
 app.listen(PORT, () => {
